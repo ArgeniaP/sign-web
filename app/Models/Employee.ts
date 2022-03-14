4127c456e20478861a74322f1c8987ffd5c0f60e
Ilham Taufik Hidayat <ilham.t.hidayat@gmail.com>
2022-03-15 00:14:32 +0700

    Add employee model & migration

---
+++ app/Models/Employee.ts
@@ -0,0 +1,25 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Employee extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public nip: string
+
+  @column()
+  public nama: string
+
+  @column()
+  public tanggalLahir: Date
+
+  @column()
+  public alamat: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
